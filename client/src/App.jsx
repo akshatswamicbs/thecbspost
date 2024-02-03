5f581f015be7b7b960e893fe46c7e0531958a0f3
akshatswamicbs <akshatswamicbs@gmail.com>
2024-02-03 19:31:11 +0530

    installed react and tailwind and est the first tempelate

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div className='text-3xl text-red-900 flex gap-3'>
+      <div className='text-blue-400'>
+        hello
+      </div>
+      <div>
+        flex box
+      </div>
+    </div>
+  )
+}
